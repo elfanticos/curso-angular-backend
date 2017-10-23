ce7d56d7b63d5f7af62f53a506efbc98a67811c7
elfanticos <email_id>
2017-10-22 22:31:24 -0500

    proyecto de angular-backend terminada

---
+++ index.php
@@ -0,0 +1,174 @@
+<?php
+	require_once 'vendor/autoload.php';
+	$app = new \Slim\Slim();
+	$db = new mysqli('localhost','root', '', 'curso_angular4');
+
+	//CONFIGURACIÓN DE CABECERAS
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+	header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+	$method = $_SERVER['REQUEST_METHOD'];
+	if($method == "OPTIONS") {
+	    die();
+	}
+
+	//LISTAR TODOS LOS PRODUCTOS
+	$app->get('/productos',function () use($db, $app) {
+		$sql = 'SELECT * FROM productos ORDER BY id DESC';
+		$query = $db->query($sql);
+		$productos = array();
+		while ($producto = $query->fetch_assoc()) {
+			$productos[] = $producto;
+		}
+		$result = array(
+			             'status' => 'success',
+			             'code'   => 200,
+			             'data'   => $productos
+		);
+		echo json_encode($result);
+	});
+
+	//DEVOLVER UN SOLO PRODUCTO
+	$app->get('/producto/:id', function($id) use($db, $app) {
+		$sql = 'SELECT * FROM productos WHERE id= '.$id;
+		$query = $db->query($sql);
+		$result = array(
+			             'status'  => 'error',
+			             'code'    => 404,
+			             'message' => 'Producto no encontrado'
+		);
+		if($query->num_rows == 1) {
+			$producto = $query->fetch_assoc();
+			$result = array(
+			             'status'  => 'success',
+			             'code'    => 200,
+			             'data' => $producto
+		    );
+		}
+		echo json_encode($result);
+	});
+	//ELIMINAR UN PRODUCTO
+	$app->get('/delete-producto/:id', function($id) use($db, $app) {
+		$sql = "DELETE FROM productos WHERE id= ".$id;
+		$query = $db->query($sql);
+		if($query) {
+			$result = array(
+				'status'  => 'success',
+				'code'    => 200,
+				'message' => 'El producto se ha eliminado correctamente!!'
+			);
+		}else {
+			$result = array (
+				'status'  => 'error',
+				'code'    => 404,
+				'message' => 'El producto no se ha eliminado!!'
+			);
+		}
+		echo json_encode($result);
+	});
+
+	//ACTUALIZAR UN PRODUCTO
+
+	$app->post('/update-producto/:id', function($id) use($db, $app) {
+		$json = $app->request->post('json');
+		$data = json_decode($json, true);
+
+
+		$sql = "UPDATE productos 
+		           SET nombre = '{$data['nombre']}',
+		               descripcion = '{$data['descripcion']}', ";
+		if(isset($data['image'])) {
+			$sql.= "image = {$data['image']},";
+		}               
+		$sql.= "       precio = {$data['precio']}
+		         WHERE id = $id";
+		$query = $db->query($sql);
+		// var_dump($sql);
+		if($query) {
+			$result = array(
+				'status'  => 'success',
+				'code'    => 200,
+				'message' => 'El producto se actualizo correctamente!!'
+			);
+		}else {
+			$result = array (
+				'status'  => 'error',
+				'code'    => 404,
+				'message' => 'El producto no se ha actualizo!!'
+			);
+		}
+		echo json_encode($result);
+	});
+	//SUBIR UNA IMAGEN A UN PRODUCTO
+	$app->post('/upload-file', function() use($db, $app) {
+		$result = array (
+				'status'  => 'error',
+				'code'    => 404,
+				'message' => 'El archivo no ha podido subirse'
+			);
+		if(isset($_FILES['uploads'])) {
+			$piramideUploader = new PiramideUploader();
+			$upload   = $piramideUploader->upload('image', 'uploads','uploads', array('image/jpeg', 'image/png', 'image/gif'));
+			$file     = $piramideUploader->getInfoFile();
+			$filename = $file['complete_name'];
+
+			if (isset($upload) && $upload['uploaded'] == false) {
+				$result = array(
+								'status ' =>'error',
+								'code'    => 404,
+								'message' => 'El archivo no ha  podido subirse'
+				);
+			}else {
+				$result = array(
+								'status '  =>'success',
+								'code'     => 200,
+								'message'  => 'El archivo se ha subido.',
+								'filename' => $filename
+				);
+			}
+			echo json_encode($result);
+		}
+	});
+	//GUARDAR PRODUCTOS
+		$app->post('/productos', function() use($db, $app) {
+		$json = $app->request->post('json');
+		$data = json_decode($json, true);
+		
+		if(!isset($data['nombre'])) {
+			$data['nombre'] = null;
+		}
+		if(!isset($data['imagen'])) {
+			$data['imagen'] = null;
+		}
+		if(!isset($data['descripcion'])) {
+			$data['descripcion'] = null;
+		}
+		if(!isset($data['precio'])) {
+			$data['precio'] = null;
+		}
+		if(!isset($data['imagen'])) {
+			$data['imagen'] = null;
+		}
+		$query = "INSERT INTO productos VALUES(NULL,".
+		          "'{$data['nombre']}',".
+		          "'{$data['descripcion']}',".
+		          "'{$data['precio']}',".
+		          "'{$data['imagen']}');";
+		// var_dump($query);
+        $result = array(
+						'status'  => 'error',
+						'code'    => 404,
+						'message' => 'Producto NO se ha creado.'
+		);
+		$insert = $db->query($query);
+		if($insert) {
+			$result = array( 'status'  => 'success',
+							 'code'    => 200,
+							 'message' => 'Producto creado correctamente.'
+			);
+		}
+		echo json_encode($result);
+	});
+	$app->run();
+?>
